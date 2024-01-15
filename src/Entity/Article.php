35dd464b3f3ef26d537a23665255a3c37867f9c6
WinceslasAdjihanou <adjihanouwinceslas@gmail.com>
2024-01-15 16:24:15 +0100

    Second commit

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -2,38 +2,100 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Metadata\ApiResource;
-use App\Repository\ArticleRepository;
+use ApiPlatform\Api\QueryParameterValidator\Validator\Length;
+use ApiPlatform\Doctrine\Odm\State\Options;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+
+use ApiPlatform\Metadata\ApiFilter;
+
+
+use AppendIterator;
+use Attribute;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\ArticleRepository;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: ArticleRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    // attributes: [
+    //     'validation_groups' => []
+    // ],
+    normalizationContext: ['groups' => ['read:collection']],
+    denormalizationContext: ['groups' => ['write:article']],
+    paginationItemsPerPage: 2,
+    paginationMaximumItemsPerPage: 2,
+    paginationClientItemsPerPage: 2,  
+),
+
+ApiFilter(SearchFilter::class, properties:['id' => 'exact', 'title' => 'partial'])
+
+
+]
 class Article
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['read:collection'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['read:collection', 'write:Article']),
+    Length(min: 8, Groups: ['create:Article'])
+    ]
+    
     private ?string $title = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['read:collection', 'write:Article'])]
     private ?string $slug = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Groups(['read:item', 'write:Article'])] 
     private ?string $content = null;
 
-    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[ORM\Column(type: Types::DATETIME_IMMUTABLE)]
+    #[Groups(['read:item' ])]
     private ?\DateTimeInterface $createdAt = null;
 
-    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[ORM\Column(type: Types::DATETIME_IMMUTABLE)]
+    
     private ?\DateTimeInterface $updatedAt = null;
+
+
+    // ... (autres propriétés)
+
+    public function getOperations(): array
+    {
+        return [
+            'put',
+            'delete',
+            'get' => [
+                'normalization_context' => ['groups' => ['read:collection', 'read:item', 'read:article', 'read:Article']],
+            ],
+        ];
+    }
+
+    #[ORM\ManyToOne(inversedBy: 'articles')]
+    #[Groups(['read:item', 'write:Article'])]
+    private ?Category $category = null;
+
+    // #[ORM\Column (Options = {"default":"0"})]
+    #[ORM\Column]
+     #[Groups(['read:collection'])]
+    private ?bool $online = false;
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+
+    public function __construct() {
+        $this->createdAt = new \DateTime();
+        $this->updatedAt = new \DateTime();
     }
 
     public function getTitle(): ?string
@@ -92,6 +154,30 @@ class Article
     public function setUpdatedAt(\DateTimeInterface $updatedAt): static
     {
         $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): static
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    public function isOnline(): ?bool
+    {
+        return $this->online;
+    }
+
+    public function setOnline(bool $online): static
+    {
+        $this->online = $online;
 
         return $this;
     }
---
+++ src/Entity/Category.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Api\QueryParameterValidator\Validator\Length;
+use ApiPlatform\Serializer\Tests\Filter\GroupFilterTest;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+#[ApiResource]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    #[Groups(['read:Article'])]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['read:Article']),
+    Length('min: 8')]
+    
+    private ?string $name = null;
+
+    #[ORM\OneToMany(mappedBy: 'category', targetEntity: Article::class)]
+    private Collection $articles;
+
+    public function __construct()
+    {
+        $this->articles = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Article>
+     */
+    public function getArticles(): Collection
+    {
+        return $this->articles;
+    }
+
+    public function addArticle(Article $article): static
+    {
+        if (!$this->articles->contains($article)) {
+            $this->articles->add($article);
+            $article->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeArticle(Article $article): static
+    {
+        if ($this->articles->removeElement($article)) {
+            // set the owning side to null (unless already changed)
+            if ($article->getCategory() === $this) {
+                $article->setCategory(null);
+            }
+        }
+
+        return $this;
+    }
+}
